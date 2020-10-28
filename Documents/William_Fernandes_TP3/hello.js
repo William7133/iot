5d3f119677536380a2a8da9963aed646f5ba7bbc
William.Fernandes <william.fernandes@ynov.com>
2020-10-28 16:49:02 +0100

    tp entier sans doc

---
+++ Documents/William_Fernandes_TP3/hello.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const host = 'localhost';
+const port = 3000;
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("{\"message\":\"Hello World\"}");
+};
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
